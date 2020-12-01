dc1d6e222fe551842c3d73d79db1923386b5b057
David Krög <david_@gmx.net>
2020-12-01 16:24:25 +0100

    added more testcases for doorstatus

--- src/test/java/at/fhhagenberg/elevatorsys/models/DoorStatusTest.java
+++ src/test/java/at/fhhagenberg/elevatorsys/models/DoorStatusTest.java
@@ -9,6 +9,8 @@ class DoorStatusTest {
     @Test
     void fromInt() {
         assertEquals(DoorStatus.CLOSED, DoorStatus.fromInt(2));
+        assertEquals(DoorStatus.OPENING, DoorStatus.fromInt(3));
+        assertEquals(DoorStatus.CLOSING, DoorStatus.fromInt(4));
         assertThrows(IllegalArgumentException.class, () -> DoorStatus.fromInt(6));
     }
 }
