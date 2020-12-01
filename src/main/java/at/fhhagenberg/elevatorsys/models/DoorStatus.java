1ae632e0002a73d2eb5af7f6f9b9043fded3f4cb
David Krög <david_@gmx.net>
2020-12-01 15:46:10 +0100

    added fromInt to DoorStatus enum

--- src/main/java/at/fhhagenberg/elevatorsys/models/DoorStatus.java
+++ src/main/java/at/fhhagenberg/elevatorsys/models/DoorStatus.java
@@ -1,8 +1,25 @@
 package at.fhhagenberg.elevatorsys.models;
 
+import static at.fhhagenberg.sqe.IElevator.*;
+
 public enum DoorStatus {
     OPEN,
     CLOSED,
     OPENING,
-    CLOSING
+    CLOSING;
+
+    public static DoorStatus fromInt(int value) {
+        switch (value) {
+            case ELEVATOR_DOORS_OPEN:
+                return OPEN;
+            case ELEVATOR_DOORS_CLOSED:
+                return CLOSED;
+            case ELEVATOR_DOORS_OPENING:
+                return OPENING;
+            case ELEVATOR_DOORS_CLOSING:
+                return CLOSING;
+            default:
+                throw new IllegalArgumentException();
+        }
+    }
 }
